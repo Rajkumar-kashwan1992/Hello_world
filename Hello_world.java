b7b02565409ff14e6881ad025409e543735bb7ce
Rajkumar-kashwan1992 <Rajkumar-kashwan1992@users.noreply.github.com>
2016-07-27 12:15:01 +0530

    Create Hello_world.java

---
+++ Hello_world.java
@@ -0,0 +1,10 @@
+import java.io.*;
+/* This is programme to print "Hello World" */
+class Hello_world{
+    public static void main(String args[]){
+      
+      System.out.println("Hello World");
+    
+    }
+
+}
